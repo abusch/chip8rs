f7459825fbb7ff05a92dd471adb244d452460c5c
Antoine Busch <antoine.busch@gmail.com>
2022-01-24 11:45:59 +1100

    Replace minifb with pixels+game-loop
    Somehow minifb stopped working properly: it blocked waiting for a
    keypress... maybe due to running on wayland?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use std::{path::Path, time::Duration};
+use std::path::Path;
 
-use anyhow::{Result, Context, bail};
+use anyhow::{bail, Context, Result};
 use clap::{App, Arg};
-use log::info;
-use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
+use game_loop::game_loop;
+use log::{error, info};
+use pixels::{Pixels, SurfaceTexture};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, VirtualKeyCode},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+use winit_input_helper::WinitInputHelper;
 
 mod config;
 mod cpu;
@@ -16,10 +24,14 @@ use gfx::Gfx;
 use interconnect::Interconnect;
 use ram::Ram;
 
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+
 /// This represents the Chip-8 virtual machine. It is composed of a `Cpu` and an `Interconnect`.
-struct Chip8 {
+pub struct Chip8 {
     cpu: Cpu,
     interconnect: Interconnect,
+    ticks: u64,
 }
 
 impl Chip8 {
@@ -38,6 +50,7 @@ impl Chip8 {
                 sound_timer: 0,
                 keys: [false; 16],
             },
+            ticks: 0,
         })
     }
 
@@ -50,15 +63,45 @@ impl Chip8 {
     }
 
     pub fn step(&mut self) {
+        self.ticks += 1;
         self.cpu.emulate_cycle(&mut self.interconnect);
+        if self.ticks == 16 {
+            self.interconnect.tick();
+            self.ticks = 0;
+        }
+    }
+}
+
+pub struct Game {
+    chip8: Chip8,
+    pixels: Pixels,
+    input: WinitInputHelper,
+}
+
+impl Game {
+    pub fn new(pixels: Pixels, chip8: Chip8) -> Result<Self> {
+        let input = WinitInputHelper::new();
+        Ok(Self {
+            chip8,
+            pixels,
+            input,
+        })
+    }
+
+    pub fn update(&mut self) {
+        self.chip8.step();
+    }
+
+    pub(crate) fn update_controls(&mut self, event: &Event<()>) {
+        self.input.update(event);
+        for (i, key) in KEYS.iter().enumerate() {
+            self.chip8.set_key(i as u8, self.input.key_held(*key));
+        }
     }
 }
 
 fn main() -> Result<()> {
-    const WIDTH: usize = 64;
-    const HEIGHT: usize = 32;
-
-    env_logger::try_init()?;
+    env_logger::init();
 
     let app = App::new("chip8rs")
         .author("Antoine Busch")
@@ -67,7 +110,7 @@ fn main() -> Result<()> {
         .arg(
             Arg::new("scale")
                 .required(false)
-                .default_value("4")
+                .default_value("8")
                 .possible_values(&["1", "2", "4", "8", "16", "32"])
                 .short('s')
                 .long("scale"),
@@ -76,84 +119,94 @@ fn main() -> Result<()> {
 
     let rom = app.value_of("ROM").expect("Missing ROM file");
     let scale = match app.value_of("scale").context("Missing scale")? {
-        "1" => Scale::X1,
-        "2" => Scale::X2,
-        "4" => Scale::X4,
-        "8" => Scale::X8,
-        "16" => Scale::X16,
-        "32" => Scale::X32,
+        "1" => 1.0,
+        "2" => 2.0,
+        "4" => 4.0,
+        "8" => 8.0,
+        "16" => 16.0,
+        "32" => 32.0,
         _ => bail!("Invalid scale factor"),
     };
 
     info!("loading rom {}", rom);
-    let mut chip8 = Chip8::new(rom)?;
+    let chip8 = Chip8::new(rom)?;
 
-    let mut buffer = vec![0u32; WIDTH * HEIGHT];
-    let mut window = Window::new(
-        "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
-        WindowOptions {
-            borderless: false,
-            title: true,
-            resize: false,
-            scale,
-            scale_mode: ScaleMode::Stretch,
-            topmost: false,
-            ..WindowOptions::default()
+    let event_loop = EventLoop::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let scaled_size = LogicalSize::new(WIDTH as f64 * scale, HEIGHT as f64 * scale);
+        WindowBuilder::new()
+            .with_title("Chip8rs -- Chip8 Emulator")
+            .with_inner_size(scaled_size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture)?
+    };
+
+    let game = Game::new(pixels, chip8)?;
+
+    game_loop(
+        event_loop,
+        window,
+        game,
+        1000,
+        0.1,
+        |g| {
+            /* update */
+            g.game.update();
         },
-    )?;
-
-    // Limit to max ~60 fps update rate
-    window.limit_update_rate(None);
-
-    let mut tick = 0;
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        tick += 1;
-        if tick > 16 {
-            // If ~16ms have elapsed (i.e 60Hz), send a tick to decrement the timers
-            chip8.interconnect.tick();
-            tick = 0;
-        }
-        chip8.step();
-
-        if chip8.interconnect.gfx.dirty {
-            buffer
-                .iter_mut()
-                .zip(chip8.gfx_buffer().iter())
-                .for_each(|(b, v)| *b = if *v == 0 { 0u32 } else { 0xFFFFFFFF });
-
-            // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-            window.update_with_buffer(&buffer, WIDTH, HEIGHT)?;
-        } else {
-            window.update();
-        }
-        for (i, key) in KEYS.iter().enumerate() {
-            chip8.set_key(i as u8, window.is_key_down(*key));
-        }
-
-        // sleep for 1ms i.e. run about 1 instruction per millisecond
-        std::thread::sleep(Duration::from_millis(1));
-    }
-
-    Ok(())
+        |g| {
+            /* render */
+            if g.game.chip8.interconnect.gfx.dirty {
+                g.game
+                    .pixels
+                    .get_frame()
+                    .chunks_exact_mut(4)
+                    .zip(g.game.chip8.gfx_buffer().iter())
+                    .for_each(|(b, v)| {
+                        if *v == 0 {
+                            b.copy_from_slice(&[0, 0, 0, 0])
+                        } else {
+                            b.copy_from_slice(&[255, 255, 255, 255])
+                        }
+                    });
+                if let Err(e) = g.game.pixels.render() {
+                    error!("Render error: {}", e);
+                    g.exit();
+                }
+            }
+        },
+        |g, event| {
+            g.game.update_controls(&event);
+            // Close events
+            if g.game.input.key_pressed(VirtualKeyCode::Escape) || g.game.input.quit() {
+                g.exit();
+            }
+        },
+    );
 }
 
-const KEYS: [Key; 16] = [
-    Key::X,
-    Key::Key1,
-    Key::Key2,
-    Key::Key3,
-    Key::Q,
-    Key::W,
-    Key::E,
-    Key::A,
-    Key::S,
-    Key::D,
-    Key::Z,
-    Key::C,
-    Key::Key4,
-    Key::R,
-    Key::F,
-    Key::V,
+const KEYS: [VirtualKeyCode; 16] = [
+    VirtualKeyCode::X,
+    VirtualKeyCode::Key1,
+    VirtualKeyCode::Key2,
+    VirtualKeyCode::Key3,
+    VirtualKeyCode::Q,
+    VirtualKeyCode::W,
+    VirtualKeyCode::E,
+    VirtualKeyCode::A,
+    VirtualKeyCode::S,
+    VirtualKeyCode::D,
+    VirtualKeyCode::Z,
+    VirtualKeyCode::C,
+    VirtualKeyCode::Key4,
+    VirtualKeyCode::R,
+    VirtualKeyCode::F,
+    VirtualKeyCode::V,
 ];
